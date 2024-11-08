7c775bd55e283be522ad2a8fb662f67a9fd3c5ff
jms-eong <mseong2741@gmail.com>
2024-11-09 02:08:55 +0900

    TextTMPViewer

--- 타오랜/Assets/Scripts/WaveSystem.cs
+++ 타오랜/Assets/Scripts/WaveSystem.cs
@@ -3,10 +3,14 @@ using UnityEngine;
 public class WaveSystem : MonoBehaviour
 {
     [SerializeField]
-    private Wave[]       waves;
+    private Wave[]       waves;                 // 현재 스테이지의 모든 웨이브 정보
     [SerializeField]
     private EnemySpawner enemySpawner;
-    private int          currentWaveIndex = -1;
+    private int          currentWaveIndex = -1; // 현재 웨이브 인덱스
+
+    // 웨이브 정보 출력을 위한 Get 프로퍼티 (현재 웨이브, 총 웨이브)
+    public int           CurrentWave => currentWaveIndex + 1;       // 시작이 0이기 때문에 +1
+    public int           MaxWave => waves.Length;
 
     public void StartWave()
     {
