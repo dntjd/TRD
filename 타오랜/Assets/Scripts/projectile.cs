8cfa60e486434d73dce5d03026a551e822a419b4
Moon9201 <7557moon@naver.com>
2024-11-09 00:13:41 +0900

    .
    .

--- 타오랜/Assets/Scripts/projectile.cs
+++ 타오랜/Assets/Scripts/projectile.cs
@@ -6,17 +6,38 @@ public class projectile : MonoBehaviour
 {
     private Movement2D movement2D;
     private Transform target;
+    private int damage;
 
     // Start is called before the first frame update
-    public void Setup(Transform target)
+    public void Setup(Transform target,int damage)
     {
         movement2D = GetComponent<Movement2D>();
         this.target = target;
+        this.damage = damage;
     }
 
     // Update is called once per frame
     private void Update()
     {
-        
+        if (target != null)
+        {
+            Vector3 direction = (target.position - transform.position).normalized;
+            movement2D.MoveTo(direction);
+        }
+        else { 
+        Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (!collision.CompareTag("Enemy")) return;
+        if (collision.transform != target) return;
+
+        //collision.GetComponent<Enemy>().Ondie();
+        collision.GetComponent<EnemyHP>().Takedamage(damage);
+        Destroy(gameObject);
+
     }
 }
