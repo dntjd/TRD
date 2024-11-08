e9bedde81da15cdcc7c123220cb77b92d1344efd
jms-eong <mseong2741@gmail.com>
2024-11-09 01:52:36 +0900

    WaveSystem 2차 수정

--- 타오랜/Assets/Scripts/WaveSystem.cs
+++ 타오랜/Assets/Scripts/WaveSystem.cs
@@ -2,6 +2,23 @@ using UnityEngine;
 
 public class WaveSystem : MonoBehaviour
 {
+    [SerializeField]
+    private Wave[]       waves;
+    [SerializeField]
+    private EnemySpawner enemySpawner;
+    private int          currentWaveIndex = -1;
+
+    public void StartWave()
+    {
+        // 현재 맵에 적이 없고, Wave가 남아있으면
+        if  ( enemySpawner.EnemyList.Count == 0 && currentWaveIndex < waves.Length-1 )
+        {
+            // 인덱스의 시작이 -1이기 때문에 웨이브 인덱스 증가를 제일 먼저 함
+            currentWaveIndex ++;
+            // EnemySpawner의 StartWave() 함수 호출. 현재 웨이브 정보 제공
+            enemySpawner.StartWave(waves[currentWaveIndex]);
+        }
+    }
 }
 
 [System.Serializable]
