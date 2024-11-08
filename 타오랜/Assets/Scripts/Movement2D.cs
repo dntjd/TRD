677ffa0833e7af74a76af7a295ccc7ba815e7b56
dntjd <jjok1021ohs@gmail.com>
2024-11-09 00:10:49 +0900

    적 이동

--- 타오랜/Assets/Scripts/Movement2D.cs
+++ 타오랜/Assets/Scripts/Movement2D.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Movement2D : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private float moveSpeed = 0.0f;
+    [SerializeField]
+    private Vector3 moveDirection = Vector3.zero;
 
-    // Update is called once per frame
-    void Update()
+    public float MoveSpeed => moveSpeed;  //moveSpeed 변수의 프로퍼티(property) (Get 가능)
+
+    private void Update()
     {
-        
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+    }
+    public void MoveTo(Vector3 direction)
+    {
+        moveDirection = direction; 
     }
 }
