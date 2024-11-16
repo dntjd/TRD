229a515c3636dec20a83aac44a662f74c384ab39
jms-eong <mseong2741@gmail.com>
2024-11-16 20:13:14 +0900

    ..

--- 타오랜/Assets/Scripts/Shop/ItemData.cs
+++ 타오랜/Assets/Scripts/Shop/ItemData.cs
@@ -2,17 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemData : MonoBehaviour
+[CreateAssetMenu(fileName = "item_Data", menuName = "Scriptable Object/item_Data", order = int.MaxValue)]
+//생성메뉴바에 item_Data라는 scriptableobject를 생성하는 메뉴를 생성
+public class ItemData : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField]
+    private int idx; // 아이템의 인덱스
+    public int IDX { get { return idx; } }
+    [SerializeField]
+    private string item_name; // 아이템의 이름
+    public string Item_Name { get { return item_name; } }
+    [SerializeField]
+    private int price; // 아이템의 가격
+    public int Price { get { return price; } }
+    [SerializeField]
 }
