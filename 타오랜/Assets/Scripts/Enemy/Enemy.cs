237a2a0852a674f8b07e8495910fe48f00cd7d30
dntjd <jjok1021ohs@gmail.com>
2024-11-19 14:46:01 +0900

    ..

--- 타오랜/Assets/Scripts/Enemy/Enemy.cs
+++ 타오랜/Assets/Scripts/Enemy/Enemy.cs
@@ -5,23 +5,25 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField]
-    protected int FullHp;
+    protected int FullHp; //최대 체력
     [SerializeField] 
-    protected int CurrentHp;
+    protected int CurrentHp; //현재 체력
     [SerializeField]
-    protected int atkDmg;
+    protected int atkDmg; //공격력(필요없다고 판단될 시 제거)
     [SerializeField]
-    protected float atkSpeed;
+    protected float atkSpeed; //공격속도(필요없다고 판단될 시 제거)
     [SerializeField]
-    protected float MoveSpeed;
-
+    protected float MoveSpeed; // 이동속도
+    [SerializeField]
+    protected int reward; //처치보상
     
     private void Start()
     {
         FullHp = 30;
-        CurrentHp = 30;
+        CurrentHp = FullHp;
         atkDmg = 10;
         atkSpeed = 2.0f;
         MoveSpeed = 5.0f;
+        reward = 10;
     }
 }
