5437d22718996126470c8c46553e1087d41dc311
dntjd <jjok1021ohs@gmail.com>
2024-11-19 17:42:10 +0900

    .

--- 타오랜/Assets/Scripts/Enemy/WaypointManager.cs
+++ 타오랜/Assets/Scripts/Enemy/WaypointManager.cs
@@ -2,17 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class WaypointManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static WaypointManager Instance; // 싱글턴 패턴
+
+    [SerializeField]
+    private Transform[] waypoints; // 경로에 있는 모든 웨이포인트
+
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// 특정 인덱스의 웨이포인트 반환
+    /// </summary>
+    /// <param name="index">웨이포인트 인덱스</param>
+    /// <returns>웨이포인트 위치</returns>
+    public Transform GetWaypoint(int index)
     {
-        
+        if (index < 0 || index >= waypoints.Length)
+        {
+            return null; // 인덱스 초과 시 null 반환
+        }
+        return waypoints[index];
     }
 }
